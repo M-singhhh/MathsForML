c223c299b27d805774b57d3b68f23f56f9f5bd99
Mohan Singh <7355184966mohan@gmail.com>
2026-02-16 01:12:44 +0530

    probablity chart for n number of people sharing bday

---
+++ prob_bday.py
@@ -0,0 +1,8 @@
+import numpy as np 
+import matplotlib.pyplot as plt 
+
+number_of_people = np.linspace(1, 100, 100) # number of people from 1 to 100
+probability = 1 - np.exp(-number_of_people**2 / (2 * 365)) # probability of at least two people sharing a birthday
+plt.plot(number_of_people, probability) # plot the probability as a function of the number of people
+plt.title('Probability of at least two people sharing a birthday')  
+plt.show() 
