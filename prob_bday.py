d06dfdef76188cb027881e13106cbfa0a49bb378
Mohan Singh <7355184966mohan@gmail.com>
2026-02-23 23:12:56 +0530

    commit

--- prob_bday.py
+++ prob_bday.py
@@ -5,4 +5,5 @@ number_of_people = np.linspace(1, 100, 100) # number of people from 1 to 100
 probability = 1 - np.exp(-number_of_people**2 / (2 * 365)) # probability of at least two people sharing a birthday
 plt.plot(number_of_people, probability) # plot the probability as a function of the number of people
 plt.title('Probability of at least two people sharing a birthday')  
+plt.grid(True)
 plt.show() 
